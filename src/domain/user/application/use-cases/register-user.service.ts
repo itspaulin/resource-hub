fca7e9c4b4aacc723981ad215f829c0d11380ff8
BalvesD3v <lpbalves7filho@gmail.com>
2025-06-10 21:08:12 -0300

    feat(user): add error handling and user use case - Added error handling for user already exists in application/use-cases - Added user use case

---
+++ src/domain/user/application/use-cases/register-user.service.ts
@@ -0,0 +1,55 @@
+import { Either, left, right } from '@/core/either';
+import { UserRole } from '../../enterprise/entities/enums/role.enum';
+import { UserAlreadyExistsError } from './errors/UserAlreadyExists';
+import { User } from '../../enterprise/entities/user';
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../repositories/user-repository';
+import { HashGenerator } from '../cryptography/hash-generator';
+
+interface RegisterUserServiceRequest {
+  name: string;
+  email: string;
+  password: string;
+  role?: UserRole;
+}
+
+type RegisterUserServiceResponse = Either<
+  UserAlreadyExistsError,
+  {
+    user: User;
+  }
+>;
+
+@Injectable()
+export class RegisterUserService {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    role = UserRole.USER,
+  }: RegisterUserServiceRequest): Promise<RegisterUserServiceResponse> {
+    const existingUser = await this.userRepository.findByEmail(email);
+
+    if (existingUser) {
+      return left(new UserAlreadyExistsError(email));
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password);
+
+    const user = User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await this.userRepository.create(user);
+
+    return right({ user });
+  }
+}
